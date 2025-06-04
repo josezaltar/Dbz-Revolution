e98435f642c80a9134b8ce998a9a33674cf88fab
zezaltar <josezaltar@gmail.com>
2025-06-04 17:59:07 -0300

    Tirando Public

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -3,7 +3,6 @@ import imagemin from "gulp-imagemin";
 import * as sass from "sass"; // Correção na importação do sass
 import gulpSass from "gulp-sass"; // Importando gulp-sass normalmente
 import gulpCopy from "gulp-copy"; // Corrigido para importar o gulp-copy corretamente
-import uglify from "gulp-uglify";
 
 // Passando o compilador para o gulp-sass
 const sassCompiler = gulpSass(sass);
@@ -12,19 +11,19 @@ function styles() {
   return gulp
     .src("./src/styles/*.scss")
     .pipe(sassCompiler({ outputStyle: "compressed" }))
-    .pipe(gulp.dest("./public/css")); // Alterado para public
+    .pipe(gulp.dest("./dist/css")); // Alterado para public
 }
 
 function images() {
   return gulp
     .src(["./src/images/**/*"], { encoding: false })
     .pipe(imagemin())
-    .pipe(gulp.dest("./public/images")); // Alterado para public
+    .pipe(gulp.dest("./dist/images")); // Alterado para public
 }
 
 // Função para copiar o index.html para a pasta public
 function copyIndexHtml() {
-  return gulp.src("./index.html").pipe(gulpCopy("./public", { prefix: 1 })); // Copia o index.html para a pasta public
+  return gulp.src("./index.html").pipe(gulpCopy("./dist", { prefix: 1 })); // Copia o index.html para a pasta public
 }
 
 // Exportando tarefas
